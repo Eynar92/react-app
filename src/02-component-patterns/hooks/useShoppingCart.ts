5dedc1ad09181b186be8790236cc6f0289c63f57
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-03-03 01:52:31 -0400

    control props alternative

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,36 +6,21 @@ export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
-        // console.log('onProductCountChange', count, product);
 
         setShoppingCart(oldShopingCart => {
 
-            // if (count === 0) {
+            if (count === 0) {
 
-            //     const { [product.id]: toDelete, ...rest } = oldShopingCart;
+                const { [product.id]: toDelete, ...rest } = oldShopingCart;
 
-            //     return rest
-            // }
-
-            // return {
-            //     ...oldShopingCart,
-            //     [product.id]: { ...product, count }
-            // }
-
-            // NUEVA IMPLEMENTACIÓN
-            const productInCart: ProductInCart = oldShopingCart[product.id] || { ...product, count: 0 };
-
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShopingCart,
-                    [product.id]: productInCart,
-                }
+                return rest
             }
 
-            // Si salta lo anterior significa que no hay producto, debemos borrarlo
-            const { [product.id]: toDelete, ...rest } = oldShopingCart;
-            return rest
+            return {
+                ...oldShopingCart,
+                [product.id]: { ...product, count }
+            }
+
         })
     }
 
